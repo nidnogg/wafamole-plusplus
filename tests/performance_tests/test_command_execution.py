60ab323d81d6d4d477e17896e6ded4a9fd13cac2
nidnogg <henriquevt98@gmail.com>
2022-07-17 22:55:20 -0300

    Finished testing runtimes

---
+++ tests/performance_tests/test_command_execution.py
@@ -0,0 +1,22 @@
+import subprocess
+import time
+
+total_runs = 500
+file_name = './test_ada.txt'
+model_location = "../../wafamole/models/custom/svc/test_ada_classifier.dump"
+# model_location = "../../wafamole/models/custom/example_models/waf-brain.h5"
+# model_location="../../wafamole/models/custom/example_models/random_forest_trained.dump"
+model_type = "svc"
+# model_type = "waf-brain"
+# model_type = "token"
+i = 0
+with open(file_name, 'a') as outfile:
+    while i < total_runs:
+        bash_cmd = ["wafamole", "evade", "--model-type", model_type, model_location, "admin' OR 1=1#"]
+        start_time = time.perf_counter()
+        process = subprocess.run(bash_cmd, stdout=outfile)
+        end_time = time.perf_counter()
+        print("{:.8} seconds".format((end_time - start_time)), file=outfile)
+        i+=1
+
+
